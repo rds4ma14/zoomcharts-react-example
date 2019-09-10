070db0337650473bb36bd07bb2b84f6bdd608a1d
rds4ma14 <estagio@samaiait.com.br>
2019-09-10 13:33:53 -0300

    license refactored

--- src/ZoomCharts.js
+++ src/ZoomCharts.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import zc from '@dvsl/zoomcharts'
-
+// import license from './License'
 const TimeChart = zc.TimeChart
+const license = require("./License.json")
 
 // Zoomcharts license and license key
-window.ZoomChartsLicense = ''
-window.ZoomChartsLicenseKey = ''
+window.ZoomChartsLicense = license.License;
+window.ZoomChartsLicenseKey = license.LicenseKey;
 
 class ZoomCharts extends Component {
+  
   componentDidMount () {
+   
     //
     var t = new TimeChart({
       container: document.getElementById('chartTimeChart'),
@@ -20,10 +23,14 @@ class ZoomCharts extends Component {
             ['2017-01-20', 200],
             ['2017-01-29', 300],
             ['2017-02-05 15:20:00', 400],
-            ['2017-02-15 22:59:59', 500]
+            ['2017-02-15 22:59:59', 500],
+            ['2017-02-18 13:59:59', 200],
+            ['2017-02-25 02:59:59', 500]
+
+
           ],
           dataLimitFrom: '2017-01-09 00:00:00',
-          dataLimitTo: '2017-02-15 22:59:59',
+          dataLimitTo: '2017-02-25 22:59:59',
           unit: 's'
         }
       }
